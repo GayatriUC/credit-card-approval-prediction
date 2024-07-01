c5ae79f33349788cbf765a507a9455fa9f8868b4
Gayatri UC <gc19051996@gmail.com>
2024-07-01 15:58:22 +0530

    app.py modified commit

--- app.py
+++ app.py
@@ -11,15 +11,19 @@ rfmodel=pickle.load(open('rfmodel.pkl','rb'))
 @app.route('/')
 def home():
   return render_template('home.html')
+
 @app.route('/predict_api', methods=['POST'])
 def predict_api():
-  data=request.json['data']
-  print(data)
-  print(np.array(list(data.values())).reshape(1,-1))
-  new_data=scaling.transform(np.array(list(data.values())).reshape(1,-1))
-  output=rfmodel.predict(new_data)
-  print(output[0])
-  return jsonify(output[0])
+  # Get data from request
+  data = request.get_json()
+  # Convert data to DataFrame
+  df = pd.DataFrame(data['features'])  # Access features array
+  # Preprocess categorical data (if necessary)
+  # ... (e.g., one-hot encoding)
+  # Make predictions
+  predictions = rfmodel.predict(df)
+  # ...
+
 
 
 if __name__=="__main__":
