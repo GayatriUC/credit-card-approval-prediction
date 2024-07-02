e4cca9dc6e9012a6b86d88b756838954c9d1fc8f
Gayatri UC <gc19051996@gmail.com>
2024-07-03 00:04:00 +0530

    app.py and home.html file updated commit

--- app.py
+++ app.py
@@ -1,29 +1,42 @@
-from flask import Flask, request, jsonify
-import pickle
+from flask import Flask, request, jsonify, render_template
 import pandas as pd
+import numpy as np
+import pickle
 
 app = Flask(__name__)
 
-# Load the trained pipeline
-with open('rfmodel.pkl', 'rb') as f:
-    pipe_RF = pickle.load(f)
+# Load the model
+with open('rfmodel.pkl', 'rb') as file:
+    rf = pickle.load(file)
+
+# Load the dataset
+data = pd.read_csv("traineddata.csv")
+
+@app.route('/')
+def home():
+    Type_Income = data['Type_Income'].unique()
+    Type_Occupation = data['Type_Occupation'].unique()
+    Marital_status = data['Marital_status'].unique()
+    Housing_type = data['Housing_type'].unique()
+  
+    return render_template('index.html', Type_Income=Type_Income, Type_Occupation=Type_Occupation, Marital_status=Marital_status , Housing_type=Housing_type)
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    # Get the data from the POST request
-    data = request.json  # Assuming the data is sent as JSON
-    new_data = pd.DataFrame(data)
+     Type_Income = request.form['Type_Income']
+     Type_Occupation = request.form['Type_Occupation']
+     Marital_status = request.form['Marital_status']
+     Housing_type = request.form['Housing_type']
+     Annual_income = float(request.form['Annual_income'])
+     age = float(request.form['age'])
+     experience = float(request.form['experience'])
     
-    # Ensure the new data contains all the required columns
-    required_columns = pipe_RF.named_steps['encoding'].transformers_[0][2] + \
-                       list(new_data.columns[len(pipe_RF.named_steps['encoding'].transformers_[0][2]):])
-    new_data = new_data.reindex(columns=required_columns, fill_value=0)
-    
-    # Preprocess and predict
-    y_new_pred = pipe_RF.predict(new_data)
-    
-    # Return the prediction as JSON
-    return jsonify({'prediction': y_new_pred.tolist()})
+
+query = pd.DataFrame(input).T
+prediction = rf.predict(query)[0]
+
+result = f"prediction is {prediction}"
+render_template('index.html', prediction_text=result)
 
 if __name__ == '__main__':
     app.run(debug=True)
